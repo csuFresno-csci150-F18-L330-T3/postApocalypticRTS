ac95f7c5129967ea026e3ed53287c39c21d01fc3
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-10-15 13:39:31 -0700

    Update CameraController.cs
    Basic WASD and mouse camera control
    Will try to add scroll wheel zoom

---
+++ CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+//Basic WASD and Mouse camera control
+//Will try to add scroll wheel zoom in/out
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 10f;
+    public float panBorderThickness = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
