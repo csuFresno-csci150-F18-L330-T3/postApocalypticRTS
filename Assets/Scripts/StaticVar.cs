e767d578fb29ee95a2e310f64677f4f1cb7abeb4
fredw21 <43050951+fredw21@users.noreply.github.com>
2018-10-21 00:31:08 -0700

    Working tiles playground

---
+++ Assets/Scripts/StaticVar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class StaticVar : MonoBehaviour {
+
+	public static Vector3Int currentCell;
+
+	// Use this for initialization
+	void Start () {
+		currentCell = new Vector3Int(10, 0, 0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
