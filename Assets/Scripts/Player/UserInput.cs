209774683ad353131b3dee798ec2b623eb154454
Steven Ramos <rsteven46@mail.fresnostate.edu>
2018-11-03 20:29:39 -0700

    Update Assets/Scripts/Player/UserInput.cs

--- Assets/Scripts/Player/UserInput.cs
+++ Assets/Scripts/Player/UserInput.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class UserInput : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private Player player;
+
+    // Use this for initialization
+    void Start () {
+        player = transform.root.GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (player.human) { }
 	}
 }
