acd041e408989176562a9fb85a55cc7aa19f53f6
Steven Ramos <rsteven46@mail.fresnostate.edu>
2018-11-06 19:53:02 -0800

    finish

--- Assets/Scripts/GameObjects/WorldObject.cs
+++ Assets/Scripts/GameObjects/WorldObject.cs
@@ -2,7 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class WorldObject : MonoBehaviour {
+
+    //public variables so that any player can access these
+    public string objectName;
+    public Texture2D buildImage;
+    public int cost, hitPoints, maxHitPoints;
+
+    //Variables accessible by subclasses
+    protected Player player;
+    protected string[] actions = { };
+    protected bool currentlySelected = false;
+
 
 	// Use this for initialization
 	void Start () {
