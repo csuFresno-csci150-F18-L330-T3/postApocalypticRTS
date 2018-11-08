7baf675a82c48d49d4cf804960e0b92d9be32d51
Steven Ramos <rsteven46@mail.fresnostate.edu>
2018-11-08 15:56:24 -0800

    Objects

---
+++ Assets/Scripts/GameObjects/Building/Building.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : Object {
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void OnGUI()
+    {
+        base.OnGui();
+    }
+}
