708bb1c49397627c45f3993c85d3325125c07e85
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-11-07 23:16:28 -0800

    Main Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
 }
