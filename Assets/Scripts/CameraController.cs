2c04239c26dfdad5c506d2108f1ff9503a8c49a0
fredw21 <43050951+fredw21@users.noreply.github.com>
2018-11-09 09:05:09 -0800

    Init start to WorldMap class
    Initial implementation added for addUnit and removeUnit. Currently able to add a redSquare or a greenSquare to the visible WorldTileMap.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,107 +5,54 @@ using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
- public float panSpeed = 10f;
-public float panBorderThickness = 5f;
+	public float panSpeed = 10f;
+	public float panBorderThickness = 5f;
 
-//Scroll wheel functions
-public float scroll;
-public float scrollSpeed = 10f;
-public Camera mainCam;
-float minZoom = 1f;
-float maxZoom = 20f;
-public Vector3 panLimit = new Vector3 (50, 50, 0);
-public Tile redSquare;
-public Tilemap worldTileMap;
+	//Scroll wheel functions
+	public float scroll;
+	public float scrollSpeed = 10f;
+	public Camera mainCam;
+	float minZoom = 1f;
+	float maxZoom = 20f;
+	public Vector3 panLimit = new Vector3 (50, 50, 0);
 
-const int WORLD_X_MIN = -50;
-const int WORLD_X_MAX = 50;
-const int WORLD_Y_MIN = -50;
-const int WORLD_Y_MAX = 50;
+	void Start(){}
 
-int[,] worldMap = new int[100, 100];
+	void Update()
+	{
+		Vector3 pos = transform.position;
+		
+		// WASD camera method
+		if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+		{
+			pos.y += panSpeed * Time.deltaTime;
+		}
 
-void Start()
-{
-    for(int i = 0; i < 100; i++){
-        for(int j = 50; j < 60; j++){
-            worldMap[i, j] = 1;
-        }
-    }
+		if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+		{
+			pos.y -= panSpeed * Time.deltaTime;
+		}
 
-    int tempPosX = WORLD_X_MIN;
-    int tempPosY = WORLD_Y_MAX;
-    for(int i = 0; i < 100; i++){
-        for(int j = 0; j < 100; j++){
-            if(worldMap[i, j] == 1){
-                worldTileMap.SetTile(new Vector3Int(tempPosX, tempPosY, 0), redSquare);
-            }
-            tempPosX++;
-        }
-        tempPosY--;
-    }
-}
+		if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+		{
+			pos.x += panSpeed * Time.deltaTime;
+		}
 
-void Update()
-{
+		if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+		{
+			pos.x -= panSpeed * Time.deltaTime;
+		}
+		
+		//Scroll method
+		scroll = Input.GetAxis("Mouse ScrollWheel");
+		float zoom = mainCam.orthographicSize + (-scroll * scrollSpeed);
+		zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+		mainCam.orthographicSize = zoom;
 
-    Vector3 pos = transform.position;
-    // WASD camera method
-    if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
-    {
-        /*if(pos.y < WORLD_Y_MAX)*/{
-            pos.y += panSpeed * Time.deltaTime;
+		//Pan Limit
+		pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+		pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
 
-            //Vector3Int posInt = Vector3Int.RoundToInt(pos);
-            //worldTileMap.SetTile(posInt, redSquare);
-            //Debug.Log("X: " + posInt.x + "; Y: " + posInt.y);
-        }
-    }
-
-    if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
-    {
-        /*if(pos.y > WORLD_Y_MIN)*/{
-            pos.y -= panSpeed * Time.deltaTime;
-
-            //Vector3Int posInt = Vector3Int.RoundToInt(pos);
-            //worldTileMap.SetTile(posInt, redSquare);
-            //Debug.Log("X: " + posInt.x + "; Y: " + posInt.y);
-        }
-    }
-
-    if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
-    {
-        /*if(pos.x < WORLD_X_MAX)*/{
-            pos.x += panSpeed * Time.deltaTime;
-
-           // Vector3Int posInt = Vector3Int.RoundToInt(pos);
-            //worldTileMap.SetTile(posInt, redSquare);
-            //Debug.Log("X: " + posInt.x + "; Y: " + posInt.y);
-        }
-    }
-
-    if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
-    {
-        /*if(pos.x > WORLD_X_MIN)*/{
-            pos.x -= panSpeed * Time.deltaTime;
-
-           // Vector3Int posInt = Vector3Int.RoundToInt(pos);
-            //worldTileMap.SetTile(posInt, redSquare);
-           // Debug.Log("X: " + posInt.x + "; Y: " + posInt.y);
-        }
-    }
-    //Scroll method
-    scroll = Input.GetAxis("Mouse ScrollWheel");
-    float zoom = mainCam.orthographicSize + (-scroll * scrollSpeed);
-    zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
-    mainCam.orthographicSize = zoom;
-
-    //Pan Limit
-    pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-    pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
-
-    transform.position = pos;
-}
-	
-	
+		transform.position = pos;
+	}
 }
---
+++ Assets/Scripts/WorldMap.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WorldMap : MonoBehaviour{
+	public Tilemap worldTileMap;
+	
+	public Tile redSquareImg;
+	public Tile greenSquareImg;
+	public Tile defaultWorldImg;
+	
+	public enum UnitType {RedSquare, GreenSquare};
+	
+	// Need unit type, and positon for unit
+	public void addUnit(Vector3Int unitPos, UnitType unitType){
+		switch(unitType){
+			case UnitType.RedSquare:
+				worldTileMap.SetTile(unitPos, redSquareImg);
+				break;
+			case UnitType.GreenSquare:
+				worldTileMap.SetTile(unitPos, greenSquareImg);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	// Need position of unit, will be replaced with flat land
+	public void removeUnit(Vector3Int unitPos){
+		worldTileMap.SetTile(unitPos, defaultWorldImg);
+	}
+	
+	// Returns unit type at current pos
+	public void getUnit(){
+		
+	}
+	
+	void Start(){
+		addUnit(new Vector3Int(3, 3, 0), UnitType.RedSquare);
+		addUnit(new Vector3Int(4, 3, 0), UnitType.GreenSquare);
+		removeUnit(new Vector3Int(4, 3, 0));
+	}
+
+	void Update(){
+	}
+}
