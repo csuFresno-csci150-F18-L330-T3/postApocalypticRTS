a9e44f073e69cd11f4f77ec00d130e745ab9291a
fredw21 <43050951+fredw21@users.noreply.github.com>
2018-11-09 23:00:51 -0800

    Disabled mouse movement and slightly cleaned WorldMap.cs code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -23,22 +23,22 @@ public class CameraController : MonoBehaviour
 		Vector3 pos = transform.position;
 		
 		// WASD camera method
-		if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+		if (Input.GetKey("w") /*|| Input.mousePosition.y >= Screen.height - panBorderThickness*/)
 		{
 			pos.y += panSpeed * Time.deltaTime;
 		}
 
-		if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+		if (Input.GetKey("s") /*|| Input.mousePosition.y <= panBorderThickness*/)
 		{
 			pos.y -= panSpeed * Time.deltaTime;
 		}
 
-		if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+		if (Input.GetKey("d") /*|| Input.mousePosition.x >= Screen.width - panBorderThickness*/)
 		{
 			pos.x += panSpeed * Time.deltaTime;
 		}
 
-		if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+		if (Input.GetKey("a") /*|| Input.mousePosition.x <= panBorderThickness*/)
 		{
 			pos.x -= panSpeed * Time.deltaTime;
 		}
--- Assets/Scripts/WorldMap.cs
+++ Assets/Scripts/WorldMap.cs
@@ -1,27 +1,86 @@
-﻿using UnityEngine;
+﻿// Needed => Method to prevent overwriting exisitng worldMap units; Method to 
+// prevent adding units when clicking on UI elements
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 public class WorldMap : MonoBehaviour{
+	
+	// Objs to link sprites via Unity GUI
 	public Tile attackerImg;
 	public Tile defenderImg;
 	public Tile wallImg;
 	public Tile groundImg;
 	
+	// Obj to link worldMap via UnitY GUI
 	public Tilemap worldMapFront;
 	
+	// Objs to access GUI buttons via Unity GUI
 	public Button btn_usAttacker, btn_usDefender, btn_usWall;
 	
+	// Constants to define dimesnsions of worldMap
 	public const int WORLD_X_MIN = -100;
 	public const int WORLD_X_MAX = 100;
 	public const int WORLD_Y_MIN = -100;
 	public const int WORLD_Y_MAX = 100;
 	
+	// Enum of all possible gameUnitTypes
 	public enum UnitType {Ground, Attacker, Defender, Wall};
-	
 	public UnitType curType = UnitType.Attacker;
 	
+	
+	void Start(){
+		// Configure OnClickListeners for used GUI buttons
+		btn_usAttacker.onClick.AddListener(OnClick_usAttacker);
+        btn_usDefender.onClick.AddListener(OnClick_usDefender);
+        btn_usWall.onClick.AddListener(OnClick_usWall);
+		
+		// Preset unitSelector btn colors to showcase Attacker as selected
+		btn_usAttacker.GetComponent<Image>().color = Color.green;
+		btn_usDefender.GetComponent<Image>().color = Color.red;
+		btn_usWall.GetComponent<Image>().color = Color.red;
+		
+		// Init worldMap with ground sprite
+		for(int tempX = WORLD_X_MIN; tempX < WORLD_X_MAX; tempX++){
+			for(int tempY = WORLD_Y_MIN; tempY < WORLD_Y_MAX; tempY++){
+				addUnit(new Vector3Int(tempX, tempY, 0), UnitType.Ground);
+			}
+		}
+	}
+	
+	void Update(){
+		// mouseLButton pressed, add whatever unit is selected via buttons
+		if(Input.GetMouseButtonDown(0)){
+			addUnit(getWorldCursorPos(), curType);
+		}
+	}
+	
+
+	// OnClickListener for UnitSelect-Attacker btn
+	void OnClick_usAttacker(){
+		curType = UnitType.Attacker;
+		btn_usAttacker.GetComponent<Image>().color = Color.green;
+		btn_usDefender.GetComponent<Image>().color = Color.red;
+		btn_usWall.GetComponent<Image>().color = Color.red;
+	}
+	
+	// OnClickListener for UnitSelect-Defender btn
+	void OnClick_usDefender(){
+		curType = UnitType.Defender;
+		btn_usAttacker.GetComponent<Image>().color = Color.red;
+		btn_usDefender.GetComponent<Image>().color = Color.green;
+		btn_usWall.GetComponent<Image>().color = Color.red;
+	}
+	
+	// OnClickListener for UnitSelect-Wall btn
+	void OnClick_usWall(){
+		curType = UnitType.Wall;
+		btn_usAttacker.GetComponent<Image>().color = Color.red;
+		btn_usDefender.GetComponent<Image>().color = Color.red;
+		btn_usWall.GetComponent<Image>().color = Color.green;
+	}
 	
 	// Need unit type, and positon for unit
 	public void addUnit(Vector3Int unitPos, UnitType unitType){
@@ -58,50 +117,5 @@ public class WorldMap : MonoBehaviour{
 		Vector3 mPosWorldF = Camera.main.ScreenToWorldPoint(mPosPixels);
 		Vector3Int mPosWorldI = Vector3Int.FloorToInt(mPosWorldF);
 		return mPosWorldI;
-	}
-	
-	void Start(){
-		btn_usAttacker.onClick.AddListener(OnClick_usAttacker);
-        btn_usDefender.onClick.AddListener(OnClick_usDefender);
-        btn_usWall.onClick.AddListener(OnClick_usWall);
-		
-		btn_usAttacker.GetComponent<Image>().color = Color.green;
-		btn_usDefender.GetComponent<Image>().color = Color.red;
-		btn_usWall.GetComponent<Image>().color = Color.red;
-		
-		for(int tempX = WORLD_X_MIN; tempX < WORLD_X_MAX; tempX++){
-			for(int tempY = WORLD_Y_MIN; tempY < WORLD_Y_MAX; tempY++){
-				addUnit(new Vector3Int(tempX, tempY, 0), UnitType.Ground);
-			}
-		}
-	}
-
-	void OnClick_usAttacker(){
-		curType = UnitType.Attacker;
-		btn_usAttacker.GetComponent<Image>().color = Color.green;
-		btn_usDefender.GetComponent<Image>().color = Color.red;
-		btn_usWall.GetComponent<Image>().color = Color.red;
-	}
-	
-	void OnClick_usDefender(){
-		curType = UnitType.Defender;
-		btn_usAttacker.GetComponent<Image>().color = Color.red;
-		btn_usDefender.GetComponent<Image>().color = Color.green;
-		btn_usWall.GetComponent<Image>().color = Color.red;
-	}
-	
-	void OnClick_usWall(){
-		curType = UnitType.Wall;
-		btn_usAttacker.GetComponent<Image>().color = Color.red;
-		btn_usDefender.GetComponent<Image>().color = Color.red;
-		btn_usWall.GetComponent<Image>().color = Color.green;
-	}
-	
-	void Update(){
-		
-		// mouseLButton pressed, add whatever unit is selected via buttons
-		if(Input.GetMouseButtonDown(0)){
-			addUnit(getWorldCursorPos(), curType);
-		}
 	}
 }
