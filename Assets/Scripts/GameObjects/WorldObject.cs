c7fb711334e443c1cd44549d696a7b030cf906c3
Steven Ramos <rsteven46@mail.fresnostate.edu>
2018-11-10 15:13:13 -0800

    commit

--- Assets/Scripts/GameObjects/WorldObject.cs
+++ Assets/Scripts/GameObjects/WorldObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldObject : MonoBehaviour {
+public class Object : MonoBehaviour {
 
     //public variables so that any player can access these
     public string objectName;
@@ -48,5 +48,28 @@ public class WorldObject : MonoBehaviour {
     public virtual void PerformAction(string actionToPerform)
     {
     }
+
+    public virtual void MouseClick(Object hitObject, Vector3 hitPoint, Player controller)
+    {
+        //only handle input if currently selected
+        if (currentlySelected && hitObject && hitObject.name != "Ground")
+        {
+            WorldObject worldObject = hitObject.transform.root.GetComponent<WorldObject>();
+
+            if (worldObject) ChangeSelection(worldObject, controller);
+        }
+    }
+
+    /*** Private worker methods ***/
+
+    private void ChangeSelection(WorldObject worldObject, Player controller)
+    {
+        //this should be called by the following line, but there is an outside chance it will not
+        SetSelection(false);
+        if (controller.SelectedObject) controller.SelectedObject.SetSelection(false);
+        controller.SelectedObject = worldObject;
+        worldObject.SetSelection(true);
+    }
+}
 
 }
--- Assets/Scripts/Player/UserInput.cs
+++ Assets/Scripts/Player/UserInput.cs
@@ -15,6 +15,44 @@ public class UserInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (player.human) { }
+        if (player.human) {
+            MouseActivity();
+        }
 	}
+    private void MouseActivity(){
+        if (Input.GetMouseButtonDown(0)) LeftMouseClick();
+        else if (Input.GetMouseButtonDown(1)) RightMouseClick();
+    }
+    private void LeftMouseClick()
+    {
+        if (player.hud.MouseInBounds())
+        {
+            GameObject hitObject = FindHitObject();
+            Vector3 hitPoint = FindHitPoint();
+            if (hitObject && hitPoint != ResourceManager.InvalidPosition)
+            {
+                if (player.SelectedObject) player.SelectedObject.MouseClick(hitObject, hitPoint, player);
+                else if (hitObject.name != "Ground")
+                {
+                    WorldObject worldObject = hitObject.transform.root.GetComponent<WorldObject>();
+                    if (worldObject)
+                    {
+                        //we already know the player has no selected object
+                        player.SelectedObject = worldObject;
+                        worldObject.SetSelection(true);
+                    }
+                }
+            }
+        }
+    }
+
+    private void RightMouseClick()
+    {
+        if (player.hud.MouseInBounds() && !Input.GetKey(KeyCode.LeftAlt) && player.SelectedObject)
+        {
+            player.SelectedObject.SetSelection(false);
+            player.SelectedObject = null;
+        }
+    }
+
 }
