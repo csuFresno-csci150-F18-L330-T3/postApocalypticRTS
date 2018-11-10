13f34f5728966b4df66b418a80c4b0c850ee855e
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-11-10 15:18:45 -0800

    update

---
+++ Assets/Scripts/GameMenu/WorldMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMap : MonoBehaviour
+{
+
+    public GameObject PopUp;
+
+    public void Open()
+    {
+        PopUp.SetActive(true);
+    }
+
+    public void Close()
+    {
+        PopUp.SetActive(false);
+    }
+}
