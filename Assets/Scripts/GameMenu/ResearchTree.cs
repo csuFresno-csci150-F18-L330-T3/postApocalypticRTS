a37c901f26a83dc33c9967b8b53a43df3693298c
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-11-10 18:20:54 -0800

    Update

---
+++ Assets/Scripts/GameMenu/ResearchTree.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ResearchTree : MonoBehaviour
+{
+
+    public GameObject popUp;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Press T hotkey to open/close research tree
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            popUp.gameObject.SetActive(!popUp.gameObject.activeSelf);
+        }
+        //Click Research Tree button to open/close research tree
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+                popUp.gameObject.SetActive(!popUp.gameObject.activeSelf);
+        }
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/GameMenu/WorldMapButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WorldMapButton : MonoBehaviour
+{
+
+    public GameObject popUp;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Press M hotkey to open/close world map
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            popUp.gameObject.SetActive(!popUp.gameObject.activeSelf);
+        }
+        //Click World Map button to open/close world map 
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+            popUp.gameObject.SetActive(!popUp.gameObject.activeSelf);
+        }
+    }
+
+}
+
+
+
