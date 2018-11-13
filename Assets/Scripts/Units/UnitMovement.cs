c055f3df3f9f84d10b736a8b11ad7d86a47acb65
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-11-13 13:36:23 -0800

    Unit Move

---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
