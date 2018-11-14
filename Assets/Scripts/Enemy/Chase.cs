9eb539def9aec81b0201ee5410b162076c78f3b3
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-11-13 22:47:54 -0800

    Enemy
    A basic enemy that will chase units

---
+++ Assets/Scripts/Enemy/Chase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{ 
+    public float speed;
+    private Transform unit;
+
+	void Start ()
+    {
+        //Set unit as gameobject to chase with the Player tag
+        unit = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	void Update ()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, unit.position, speed * Time.deltaTime);
+	}
+}
