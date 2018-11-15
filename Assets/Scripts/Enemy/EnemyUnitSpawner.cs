ae042c07f70aafd8bf476266523503a053dfb026
Martin31 <17520935+Martin31@users.noreply.github.com>
2018-11-14 16:42:33 -0800

    Player and Enemy scripts
    Scripts made to spawn units

---
+++ Assets/Scripts/Enemy/EnemyUnitSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnitSpawner : MonoBehaviour
+{
+
+    public GameObject enemyUnitPrefab;
+    GameObject enemyUnitPrefabClone;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            enemyUnitPrefabClone = Instantiate(enemyUnitPrefab, transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+}
