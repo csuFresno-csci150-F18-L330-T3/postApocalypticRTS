4972f0eefbcd5147c3aec2fe4a963f03b4099fe4
Martin31 <17520935+Martin31@users.noreply.github.com>
2018-11-21 11:42:12 -0800

    Made the building creator script

---
+++ Assets/Scripts/WorldObjects/Building/BuildingCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCreator : MonoBehaviour {
+
+    public GameObject buildingPrefab;
+    GameObject buildingPrefabClone;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.B)) {
+            buildingPrefabClone = Instantiate(buildingPrefab, transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+}
