5472e6abac76a526895a51b1963a9135b75eac23
fredw21 <43050951+fredw21@users.noreply.github.com>
2018-11-21 23:16:31 -0800

    Added ability to locate a unit closest to the mouse cursor
    Updated UnitSelection.cs so that a player unit closest to the mouse cursor, assuming US mode enabled and mouse click, is detected and "selected".

--- Assets/Scripts/Units/UnitSelection.cs
+++ Assets/Scripts/Units/UnitSelection.cs
@@ -36,87 +36,19 @@ public class UnitSelection : MonoBehaviour
                 Vector3Int mPosWorldI = Vector3Int.FloorToInt(mPosWorldF);
 
                 Debug.Log("Mouse L_Button click at x = " + mPosWorldI.x + ", y = " + mPosWorldI.y);
+
+                GameObject[] pUnits = GameObject.FindGameObjectsWithTag("PlayerUnit");
+
+                for(int i = 0; i < pUnits.Length; i++)
+                {
+                    Vector3Int puPosI = Vector3Int.FloorToInt(pUnits[i].transform.position);
+                    if(mPosWorldI.x == puPosI.x && mPosWorldI.y == puPosI.y)
+                    {
+                        Debug.Log("Closet unit found at x = " + puPosI.x + ", y = " + puPosI.y);
+                        break;
+                    }
+                }
             }
         }
-
-        /*if(Input.GetKey(KeyCode.Q))
-        {
-            Debug.Log("Unit select mode enabled...");
-
-            // Get curCursorPos as a Vec3Int of worldCoords
-            Vector3 mPosPixels = Input.mousePosition;
-            Vector3 mPosWorldF = Camera.main.ScreenToWorldPoint(mPosPixels);
-            Vector3Int mPosWorldI = Vector3Int.FloorToInt(mPosWorldF);
-
-        }*/
     }
-
-
-
-
-
-
 }
-/*
-    void Update()
-    {
-        GameObject[] pUnits = GameObject.FindGameObjectsWithTag("PlayerUnit");
-
-        if (totPUnits < pUnits.Length)
-        {
-            totPUnits = pUnits.Length;
-            Debug.Log("Total Player Units = " + totPUnits);
-        }
-    }
-
-
-    void Update()
-    {
-        // mouseLButton pressed, add whatever unit is selected via buttons
-        if (Input.GetMouseButtonDown(0))
-        {
-            addUnit(getWorldCursorPos(), curType);
-        }
-    }
-
-    // Need unit type, and positon for unit
-    public void addUnit(Vector3Int unitPos, UnitType unitType)
-    {
-
-        // Check to see if desired location is out of bounds
-        if (unitPos.x < WORLD_X_MIN || unitPos.x > WORLD_X_MAX || unitPos.y < WORLD_Y_MIN || unitPos.y > WORLD_Y_MAX)
-        {
-            // Desired location out of bounds, don't add units
-            Debug.Log("[WorldMap]=> Can't add unit at (" + unitPos.x + ", " + unitPos.y + "); Out of Bounds...");
-            return;
-        }
-
-        // Add desired unit's img to visible world
-        switch (unitType)
-        {
-            case UnitType.Attacker:
-                worldMapFront.SetTile(unitPos, attackerImg);
-                break;
-            case UnitType.Defender:
-                worldMapFront.SetTile(unitPos, defenderImg);
-                break;
-            case UnitType.Wall:
-                worldMapFront.SetTile(unitPos, wallImg);
-                break;
-            case UnitType.Ground:
-                worldMapFront.SetTile(unitPos, groundImg);
-                break;
-            default:
-                break;
-        }
-    }
-
-    // Get curCursorPos as a Vec3Int of worldCoords
-    public Vector3Int getWorldCursorPos()
-    {
-        Vector3 mPosPixels = Input.mousePosition;
-        Vector3 mPosWorldF = Camera.main.ScreenToWorldPoint(mPosPixels);
-        Vector3Int mPosWorldI = Vector3Int.FloorToInt(mPosWorldF);
-        return mPosWorldI;
-    }
-}*/
