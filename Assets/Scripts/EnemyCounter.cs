79976a643970628e4ee34e4661d4b342a37236bc
fredw21 <43050951+fredw21@users.noreply.github.com>
2018-11-21 22:08:10 -0800

    Total enemy unit counter
    Implementation of a script that computes a list of all enemy prefabs within a scene and outputs that total number to the console. The working list serves as a starting point to unitSelection.

---
+++ Assets/Scripts/EnemyCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCounter : MonoBehaviour
+{
+    public int totEUnits = 0;
+
+    void Start() { }
+
+    void Update()
+    {
+        GameObject[] eUnits = GameObject.FindGameObjectsWithTag("EnemyUnit");
+        
+        if(totEUnits < eUnits.Length)
+        {
+            totEUnits = eUnits.Length;
+            Debug.Log("Total Enemy Units = " + totEUnits);
+        }
+    }
+}
