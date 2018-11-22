36a22d551712d25d8b481ca53351320b2be886c4
Martin31 <17520935+Martin31@users.noreply.github.com>
2018-11-21 18:42:05 -0800

    Update BuildingCreator.cs
    It can now be used with tags in the inspector.

--- Assets/Scripts/WorldObjects/Building/BuildingCreator.cs
+++ Assets/Scripts/WorldObjects/Building/BuildingCreator.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class BuildingCreator : MonoBehaviour {
 
     public GameObject buildingPrefab;
+    GameObject[] buildingSpace;
     GameObject buildingPrefabClone;
+    public bool useTag = false;
+    public string myTag;
+    
 	// Use this for initialization
 	void Start () {
-		
+		if (useTag == true && myTag != "")//this should place a building in a place with the indicated tag
+        {
+            
+            buildingSpace = GameObject.FindGameObjectsWithTag(myTag);
+            foreach(GameObject myBuilding in buildingSpace)
+            {
+                Instantiate(buildingPrefab, myBuilding.transform.position, myBuilding.transform.rotation);
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.B)) {
+        if (Input.GetKeyDown(KeyCode.B) && useTag == false) {
             buildingPrefabClone = Instantiate(buildingPrefab, transform.position, Quaternion.identity) as GameObject;
         }
     }
