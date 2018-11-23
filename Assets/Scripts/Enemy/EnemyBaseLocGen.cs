49e060cc9def4a0daea7f23e8161ee562bcbaf29
Martin31 <17520935+Martin31@users.noreply.github.com>
2018-11-22 21:23:08 -0800

    Enemy Base Generation
    made it possible to randomly place enemy buildings

---
+++ Assets/Scripts/Enemy/EnemyBaseLocGen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaseLocGen : MonoBehaviour {
+    public float minX= -250.0f, maxX= 250.0f, minY=-130.0f, maxY= 130.0f;
+    public float numberOfBases = 10;
+    public GameObject buildingContainerPrefab;
+    //GameObject[] buildingContainerSpace;
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < numberOfBases; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
+            Instantiate(buildingContainerPrefab, position, Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
