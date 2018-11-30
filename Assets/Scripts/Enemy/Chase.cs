4d3aff3838b4c941eea5f3de256312f485dba385
Martin31 <17520935+Martin31@users.noreply.github.com>
2018-11-30 08:54:38 -0800

    Update Chase.cs
    The enemies will now go after the bases if instead of the player units if they get close to it.

--- Assets/Scripts/Enemy/Chase.cs
+++ Assets/Scripts/Enemy/Chase.cs
@@ -7,8 +7,33 @@ public class Chase : MonoBehaviour
     public float speed = 3f;
     
     public float minDist = 7f;
-    private float range;
-
+    public float minDist_b = 5f;
+    private float range, range_b;
+    public GameObject FindClosestBase()
+    {
+        GameObject[] pbase;
+        pbase = GameObject.FindGameObjectsWithTag("PlayerBase");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        if (pbase == null) {
+            //lose condition here.
+        }
+        else
+        {
+            foreach (GameObject go in pbase)
+            {
+                Vector3 diff = go.transform.position - position;
+                float curDistance = diff.sqrMagnitude;
+                if (curDistance < distance)
+                {
+                    closest = go;
+                    distance = curDistance;
+                }
+            }
+        }
+        return closest; //returns the closests object
+    }
     public GameObject FindClosestUnit()
     {
         GameObject[] unit;
@@ -44,9 +69,13 @@ public class Chase : MonoBehaviour
         // check distance between unit and enemy
         
         range = Vector2.Distance(transform.position, FindClosestUnit().transform.position);
-        
+        range_b = Vector2.Distance(transform.position, FindClosestUnit().transform.position);
 
-        if (range < minDist)
+        if (range_b < minDist_b)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, FindClosestBase().transform.position, speed * Time.deltaTime);
+        }
+        else if (range < minDist)
         {
             //Enemy movement based on unit position
             transform.position = Vector2.MoveTowards(transform.position, FindClosestUnit().transform.position, speed * Time.deltaTime);
