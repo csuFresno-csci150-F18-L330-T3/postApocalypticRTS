efcdcb665c9f8f04150496a48832cc649ad1ca65
Martin31 <17520935+Martin31@users.noreply.github.com>
2018-12-01 14:36:52 -0800

    Update UnitSpawner.cs
    The unit cap can now be set.

--- Assets/Scripts/WorldObjects/Building/UnitSpawner.cs
+++ Assets/Scripts/WorldObjects/Building/UnitSpawner.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitSpawner : MonoBehaviour {
-
+    public int unitCap = 5;
     public GameObject playerUnitPrefab;
     GameObject playerUnitPrefabClone;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
+    int NumberOfBases()
+    {
+        GameObject[] bases;
+        bases = GameObject.FindGameObjectsWithTag("PlayerBase");
+        return bases.Length;
+    }
+    int NumberOfUnits()
+    {
+        GameObject[] unit;
+        unit = GameObject.FindGameObjectsWithTag("PlayerUnit");
+        return unit.Length;
+    }
+
+    void SpawnUnit()
+    {
+        playerUnitPrefabClone = Instantiate(playerUnitPrefab, transform.position, Quaternion.identity) as GameObject;
+    }
+
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.U)) {
-            playerUnitPrefabClone = Instantiate(playerUnitPrefab, transform.position, Quaternion.identity) as GameObject;
+        if (Input.GetKeyDown(KeyCode.U) && NumberOfUnits() < unitCap * NumberOfBases()) {
+            SpawnUnit();
         }
     }
 }
