c6d2cf517057676e8803efb845feb6a9acb07b70
Martin31 <17520935+Martin31@users.noreply.github.com>
2018-12-01 17:21:09 -0800

    Fixed Chase.cs and Added a way to make builders

---
+++ Assets/Scripts/Units/CollectBlueprint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectBlueprint : MonoBehaviour {
+    public int numberOfItemsToBeCollected = 1;
+    int numberOfItemsCollected = 0;
+    public GameObject UnitPrefab;
+    GameObject UnitPrefabClone;
+    
+    void SpawnUnit()
+    {
+        UnitPrefabClone = Instantiate(UnitPrefab, transform.position, Quaternion.identity) as GameObject;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var hit = collision.gameObject;
+        if (hit.CompareTag("BluePrint"))
+        {
+            numberOfItemsCollected++;
+            Destroy(hit);
+            if (numberOfItemsCollected >= numberOfItemsToBeCollected)
+            {
+                numberOfItemsCollected = numberOfItemsCollected - numberOfItemsToBeCollected;
+                SpawnUnit();
+            }
+        }
+    }
+    
+}
