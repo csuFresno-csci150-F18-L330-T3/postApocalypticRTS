6101ea34f3a60e48c68438de39a964037e57cd4d
Steven Ramos <rsteven46@mail.fresnostate.edu>
2018-12-03 20:05:55 -0800

    Fixed code
    Made a script for the units added to prefab so it detects the boxes.
    Added prefab for the collectibles/parts

--- Assets/Scripts/Player/PlayerPickUp.cs
+++ Assets/Scripts/Player/PlayerPickUp.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerPickUp : MonoBehaviour {
 
+    public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
+    public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
 
-  
+    private int count;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,9 +30,20 @@ public class PlayerPickUp : MonoBehaviour {
             //Destroy(other.gameObject);
             other.gameObject.SetActive(false);
 
-
+            count = count + 1;
+            SetCountText();
         }
 
+    }
+    void SetCountText()
+    {
+        //Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
+        countText.text = "Count: " + count.ToString();
+
+        //Check if we've collected all 12 pickups. If we have...
+        if (count >= 5)
+            //... then set the text property of our winText object to "You win!"
+            winText.text = "You win!";
     }
 
 }
