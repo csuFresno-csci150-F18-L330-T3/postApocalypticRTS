644c650a494adf77756a1b9811d327f0a57d1045
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-12-03 18:42:21 -0800

    WorldMap Added
    Press M or the click the World map button to bring up the world map

---
+++ Assets/Scripts/Cameras/WorldMapCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WorldMapCamera : MonoBehaviour {
+
+
+    public Transform camera;
+
+
+    void LateUpdate()
+    {
+        Vector3 newPos = camera.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+    }
+
+}
+
+
+
+
+
