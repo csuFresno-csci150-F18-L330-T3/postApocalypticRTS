9a072a9ca89e121f4367f6108ed9a8371725917b
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-12-05 22:02:11 -0800

    Win Screen and conter for PARTS to win game

---
+++ Assets/Scripts/MainMenu/Win.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
