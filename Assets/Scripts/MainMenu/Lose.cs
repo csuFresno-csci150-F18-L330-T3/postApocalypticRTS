3d06636faea85b3a03d997c207d907ad7d722676
JaceBlack <43050932+JaceBlack@users.noreply.github.com>
2018-12-08 15:14:37 -0800

    Lose Screen

---
+++ Assets/Scripts/MainMenu/Lose.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lose : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
